fa910bb1bf0225f7a1c8541a8a87190301da6f30
Revellison <andrey.akimov2014@gmail.com>
2025-03-05 20:03:22 +0600

    fh fix 7

--- js/loadfooter.js
+++ js/loadfooter.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("/footer/footer.html")
+    fetch("footer/footer.html")
         .then(response => {
             if (!response.ok) {
                 throw new Error("Network response was not ok");
